85f81b6405417ec867cb6a35982df421a2ca78c0
kryptoTomo <tomasz.madej101@gmail.com>
2024-04-21 09:15:35 +0200

    some optimalization

--- app.py
+++ app.py
@@ -1,36 +1,10 @@
 import os
-import pprint
 import spotipy
+import argparse
 from dotenv import load_dotenv
 
 import AIPlaylistGenerator
 
-# load_dotenv()
-
-# sp = spotipy.Spotify(
-#     auth_manager=spotipy.SpotifyOAuth(
-#         client_id=os.getenv("SPOTIFY_CLIENT_ID"),
-#         client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
-#         redirect_uri=os.getenv("SPOTIFY_REDIRECT_URI"),
-#         scope="playlist-modify-private"
-#     )
-# )
-
-# current_user = sp.current_user()
-# assert current_user is not None
-
-# search_result = sp.search(q="Uptown Funk", type="track", limit=10)
-# pprint.pprint(search_result["tracks"]["items"][0]["id"])
-
-# tracks = [search_result["tracks"]["items"][0]["id"]]
-
-# created_playlist = sp.user_playlist_create(
-#     current_user["id"],
-#     public=False,
-#     name="TESTING PLAYLIST FUN"
-# )
-
-# sp.user_playlist_add_tracks(current_user["id"], created_playlist["id"], tracks)
 class SpotfiyAPI:
     def __init__(self) -> None:
         self.ai_playlist_generator = AIPlaylistGenerator.AIPlaylistGenerator()
@@ -48,15 +22,32 @@ class SpotfiyAPI:
         self.current_user = self.sp.current_user()
 
         assert self.current_user is not None
-        playlist = self.ai_playlist_generator.generate_playlist("epic songs", 4)
+
+    def generate_spotify_playlist(self, prompt, num_of_songs, playlist_title):
+        playlist = self.ai_playlist_generator.generate_playlist(prompt, num_of_songs)
         track_ids=[]
         for item in playlist:
             artist, song = item["artist"], item["song"]
             query = f"{song} {artist}"
-            search_result = self.sp.search(q="Uptown Funk", type="track", limit=10)
-            track_ids.append(tracks = [search_result["tracks"]["items"][0]["id"]])
+            search_result = self.sp.search(q=query, type="track", limit=10)
+            track_ids.append(search_result["tracks"]["items"][0]["id"])
             
+        created_playlist = self.sp.user_playlist_create(
+            self.current_user["id"],
+            public=False,
+            name=playlist_title
+        )
         self.sp.user_playlist_add_tracks(self.current_user["id"], created_playlist["id"], track_ids)
 
-if __name__ == "__main__":
+def main():
+    parser = argparse.ArgumentParser(description="Simple command line song utility")
+    parser.add_argument("-p", type=str, help="The prompt to describe the playlist")
+    parser.add_argument("-n", type=int, default=5, help="The number of songs to add to the playlist")
+    parser.add_argument("-t", type=str, default="TESTING PLAYLIST FUN", help="Generated playlist title")
+    args = parser.parse_args()
+
     spotify = SpotfiyAPI()
+    spotify.generate_spotify_playlist(args.p, args.n, args.t)
+
+if __name__ == "__main__":
+    main()
